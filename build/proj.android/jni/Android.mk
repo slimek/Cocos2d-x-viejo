1a61f6c29ffe31c9897c471e44fcc3822dc6e571
slimek <slime573@gmail.com>
2013-12-16 19:00:19 +0800

    [NDK] Split obj and lib directories to Debug / Release - Add -j 4 make option.

--- build/proj.android/jni/Android.mk
+++ build/proj.android/jni/Android.mk
@@ -31,13 +31,19 @@ CCDENSHION_BUILT_MODULE := $(LOCAL_BUILT_MODULE)
 
 COCOS_LIB := ../../lib
 COCOS_LIB_NDK := $(COCOS_LIB)/Android.Ndk
+COCOS_LIB_NDK_CFG := $(COCOS_LIB_NDK)/$(COCOS_CONFIG)
 COCOS_THIRD_PREBUILT := ../../cocos2dx/platform/third_party/android/prebuilt
 
 COCOS_ARCH_ABI := $(TARGET_ARCH_ABI)
 
+# $(call cocos-copy,lib-filename)
+define cocos-copy
+  $(call host-cp,obj/$(COCOS_CONFIG)/local/$@/$1,$(COCOS_LIB_NDK_CFG)/$@/$1)
+endef
+
 # $(call third-copy,lib-name)
 define third-copy
-  $(call host-cp,$(COCOS_THIRD_PREBUILT)/$1/libs/$@/$1.a,$(COCOS_LIB_NDK)/$@/$1.a)
+  $(call host-cp,$(COCOS_THIRD_PREBUILT)/$1/libs/$@/$1.a,$(COCOS_LIB_NDK_CFG)/$@/$1.a)
 endef
 
 all: $(COCOS_ARCH_ABI)
@@ -45,10 +51,11 @@ all: $(COCOS_ARCH_ABI)
 $(COCOS_ARCH_ABI): $(COCOS2DX_BUILT_MODULE) $(CCDENSHION_BUILT_MODULE)
 	$(call host-mkdir,$(COCOS_LIB))
 	$(call host-mkdir,$(COCOS_LIB_NDK))
-	$(call host-mkdir,$(COCOS_LIB_NDK)/$@)
-	$(call host-cp,obj/local/$@/libcocos2d.a,$(COCOS_LIB_NDK)/$@/libcocos2d.a)
-	$(call host-cp,obj/local/$@/libcocosdenshion.a,$(COCOS_LIB_NDK)/$@/libcocosdenshion.a)
-	$(call host-cp,obj/local/$@/libcpufeatures.a,$(COCOS_LIB_NDK)/$@/libcpufeatures.a)
+	$(call host-mkdir,$(COCOS_LIB_NDK_CFG))
+	$(call host-mkdir,$(COCOS_LIB_NDK_CFG)/$@)
+	$(call cocos-copy,libcocos2d.a)
+	$(call cocos-copy,libcocosdenshion.a)
+	$(call cocos-copy,libcpufeatures.a)
 	$(call third-copy,libjpeg)
 	$(call third-copy,libpng)
 	$(call third-copy,libtiff)
@@ -62,7 +69,7 @@ $(COCOS_ARCH_ABI): $(COCOS2DX_BUILT_MODULE) $(CCDENSHION_BUILT_MODULE)
 clean: $(COCOS_ARCH_ABI)-clean
 
 $(COCOS_ARCH_ABI)-clean:
-	$(call host-rm,$(COCOS_LIB)/Android.Ndk/$(subst -clean,,$@)/*.a)
+	$(call host-rm,$(COCOS_LIB_NDK_CFG)/$(subst -clean,,$@)/*.a)
 
 
 #
